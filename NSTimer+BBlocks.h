51a5d60c809506756d09412394c029644e64f7ef
David Keegan <git@davidkeegan.com>
2012-03-12 23:30:42 -0700

    NSTimer blocks
    Inspired by https://github.com/jivadevoe/NSTimer-Blocks

---
+++ NSTimer+BBlocks.h
@@ -0,0 +1,19 @@
+//
+//  NSTimer+BBlocks.h
+//  BBlock
+//
+//  Created by David Keegan on 3/12/12.
+//  Copyright 2012 David Keegan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSTimer(BBlocks)
+
++ (id)timerWithTimeInterval:(NSTimeInterval)timeInterval andBlock:(void (^)())block;
++ (id)timerRepeats:(BOOL)repeats withTimeInterval:(NSTimeInterval)timeInterval andBlock:(void (^)())block;
+
++ (id)scheduledTimerWithTimeInterval:(NSTimeInterval)timeInterval andBlock:(void (^)())block;
++ (id)scheduledTimerRepeats:(BOOL)repeats withTimeInterval:(NSTimeInterval)timeInterval andBlock:(void (^)())block;
+
+@end
---
+++ NSTimer+BBlocks.m
@@ -0,0 +1,54 @@
+//
+//  NSTimer+BBlocks.m
+//  BBlock
+//
+//  Created by David Keegan on 3/12/12.
+//  Copyright 2012 David Keegan. All rights reserved.
+//
+
+#import "NSTimer+BBlocks.h"
+
+@implementation NSTimer(BBlocks)
+
++ (void)_executeSimpleBlock:(NSTimer *)timer{
+    if([timer userInfo]){
+        void (^block)() = (void (^)())[timer userInfo];
+        block();
+    }
+}
+
++ (id)timerWithTimeInterval:(NSTimeInterval)timeInterval andBlock:(void (^)())block{
+    return [[self class] timerRepeats:NO withTimeInterval:timeInterval andBlock:block];
+}
+
++ (id)timerRepeats:(BOOL)repeats withTimeInterval:(NSTimeInterval)timeInterval andBlock:(void (^)())block{
+    void (^_block)() = [block copy];
+    id timer = [self timerWithTimeInterval:timeInterval 
+                                    target:self 
+                                  selector:@selector(_executeSimpleBlock:) 
+                                  userInfo:_block 
+                                   repeats:repeats];
+#if !__has_feature(objc_arc)
+    [_block release];
+#endif
+    return timer;
+}
+
++ (id)scheduledTimerWithTimeInterval:(NSTimeInterval)timeInterval andBlock:(void (^)())block{
+    return [[self class] scheduledTimerRepeats:NO withTimeInterval:timeInterval andBlock:block];
+}
+
++ (id)scheduledTimerRepeats:(BOOL)repeats withTimeInterval:(NSTimeInterval)timeInterval andBlock:(void (^)())block{
+    void (^_block)() = [block copy];
+    id timer = [self scheduledTimerWithTimeInterval:timeInterval 
+                                             target:self 
+                                           selector:@selector(_executeSimpleBlock:) 
+                                           userInfo:_block 
+                                            repeats:repeats];
+#if !__has_feature(objc_arc)
+    [_block release];
+#endif
+    return timer;
+}
+
+@end
