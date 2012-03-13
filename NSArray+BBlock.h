9f0b0e4f4113c4855db37d0fc9daee098930dfb3
David Keegan <git@davidkeegan.com>
2012-03-12 23:29:55 -0700

    Update header

--- NSArray+BBlock.h
+++ NSArray+BBlock.h
@@ -3,7 +3,7 @@
 //  BBlock
 //
 //  Created by David Keegan on 3/7/12.
-//  Copyright (c) 2012 David Keegan. All rights reserved.
+//  Copyright 2012 David Keegan. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
