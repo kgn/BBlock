ce2ff0679b7264f9db76bf7ba42a216c720a359b
David Keegan <git@davidkeegan.com>
2012-04-19 23:59:51 -0700

    Added BBUISwipeGestureRecognizer

---
+++ BBUISwipeGestureRecognizer.h
@@ -0,0 +1,16 @@
+//
+//  BBUISwipeGestureRecognizer.h
+//  BBlock
+//
+//  Created by David Keegan on 12/29/11.
+//  Copyright (c) 2011-12 David Keegan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBUISwipeGestureRecognizer : UISwipeGestureRecognizer
+
+typedef void (^BBUISwipeGestureRecognizerAction)(BBUISwipeGestureRecognizer *);
+- (id)initForDirection:(UISwipeGestureRecognizerDirection)direction withAction:(BBUISwipeGestureRecognizerAction)action;
+
+@end
