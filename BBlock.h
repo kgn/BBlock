195f6289092ac31e9b50c45e3457fade446eec5b
David Keegan <dkeegan@signnow.com>
2012-06-06 01:06:32 -0700

    Changing the self define in BBLock to be weak.
    Thanks to @Larcus94 for suggestion this solution!

--- BBlock.h
+++ BBlock.h
@@ -8,12 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
-#define BBlockSelf __block typeof(self)
-#define BBlockTypeOf(o) __block typeof(o)
+/// For when you need a weak reference to self, example: `BBlockWeakSelf wself = self;`
+#define BBlockWeakSelf __weak typeof([self weakReference])
 
-@interface BBlock : NSObject
+/// For when you need a weak reference of an object, example: `BBlockWeakObject(obj) wobj = obj;`
+#define BBlockWeakObject(o) __weak typeof([o weakReference])
+
+
+@interface BBlock
 
 /// Execute the block on the main thread
 + (void)dispatchOnMainThread:(void (^)())block;
 
+@end
+
+
+@interface NSObject(BBlockWeakReference)
+- (instancetype)weakReference;
 @end
--- BBlock.m
+++ BBlock.m
@@ -11,10 +11,20 @@
 @implementation BBlock
 
 + (void)dispatchOnMainThread:(void (^)())block{
-    NSParameterAssert(block != nil);    
+    NSParameterAssert(block != nil);
     dispatch_async(dispatch_get_main_queue(), ^{
         block();
     });
 }
 
+@end
+
+
+// Thanks to Larcus94 for suggesting this solution for creating a generic weak self define!
+// http://stackoverflow.com/questions/10892361/generic-typeof-for-weak-self-references#comment14215007_10896047
+@implementation NSObject(BBlockWeakReference)
+- (instancetype)weakReference{
+    __weak id obj = self; 
+    return obj;
+}
 @end
