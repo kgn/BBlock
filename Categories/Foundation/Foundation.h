91d25ab52d138dc8c426ee63f0d007b28dc350c5
Brad Bergeron <bradbergeron@gmail.com>
2013-10-20 12:14:53 -0400

    Adding headers to include all categories for a specified framework.

---
+++ Categories/Foundation/Foundation.h
@@ -0,0 +1,24 @@
+//
+//  Foundation.h
+//  BBlock
+//
+//  Created by Bradley David Bergeron on 10/20/13.
+//  Copyright 2013 Bradley David Bergeron. All rights reserved.
+//
+
+#ifndef _BBlock_Foundation_h
+#define _BBlock_Foundation_h
+
+#ifdef __MAC_OS
+#import "NSApplication+BBlock.h"
+#import "NSButton+BBlock.h"
+#import "NSImage+BBlock.h"
+#endif
+
+#import "NSArray+BBlock.h"
+#import "NSDictionary+BBlock.h"
+#import "NSObject+BBlock.h"
+#import "NSTimer+BBlock.h"
+#import "NSURL+BBlock.h"
+
+#endif
---
+++ Categories/StoreKit/StoreKit.h
@@ -0,0 +1,15 @@
+//
+//  StoreKit.h
+//  BBlock
+//
+//  Created by Bradley David Bergeron on 10/20/13.
+//  Copyright 2013 Bradley David Bergeron. All rights reserved.
+//
+
+#ifndef _BBlock_StoreKit_h
+#define _BBlock_StoreKit_h
+
+#import "SKProductsRequest+BBlock.h"
+#import "SKStoreProductViewController+BBlock.h"
+
+#endif
